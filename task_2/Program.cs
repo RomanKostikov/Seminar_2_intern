d7f34467117c96821c78c6cdaa16497dfbb68016
Roman <romankos1993@gmail.com>
2022-10-04 20:40:04 +0300

    task_2 completed

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -3,3 +3,8 @@
 // 782 -> 72
 // 918 -> 98
 
+Console.Clear();
+int number = new Random().Next(100, 1000);
+int first_symbol = number / 100;
+int last_symbol = number % 100;
+Console.WriteLine($"Число {number} сократилось до {first_symbol}{last_symbol}");
